9c066672f201cb99039b7bbc920aeec84bfd1db4
Branden Schrenk <schrenk.br@gmail.com>
2023-10-24 11:56:46 -0400

    feat: add a chip component

---
+++ src/components/Chip/Chip.tsx
@@ -0,0 +1,53 @@
+import React, { FormEventHandler, MouseEventHandler } from 'react';
+import styled from 'styled-components';
+
+const setDisabledBackground = (disabled: boolean | undefined) => {
+    return disabled ? '#CECECE' : '#FFFFFF';
+}
+
+const StyledChip = styled.span<{ disabled?: boolean }>`  
+    border: .1rem solid ${(props) => props.theme.colors.primary};
+    background: ${(props) => setDisabledBackground(props.disabled)};
+    display: flex;
+    padding: .7rem 1.5rem;
+    margin: .5rem;
+    border-radius: 1.5rem;
+    cursor: pointer;
+    user-select: none;
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+export interface ChipProps {
+    label: string,
+    disabled: boolean,
+    id?: string,
+    onClick?: MouseEventHandler
+}
+
+const Chip = ({
+    label,
+    disabled,
+    id,
+    onClick
+}: ChipProps) => {
+    const handleClick: MouseEventHandler = (event) => {
+        if (!disabled && onClick) {
+            onClick(event);
+        }
+    };
+
+    return (
+        <StyledChip onClick={handleClick} aria-disabled={disabled} id={id} disabled={disabled}>
+            {label}
+        </StyledChip>
+    );
+};
+
+Chip.defaultProps = {
+    disabled: false,
+};
+
+export { Chip };
